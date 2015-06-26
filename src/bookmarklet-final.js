4ff04143f241459141edbd761b42777a8150aa03
Aidan Smyth <smyth.aidan@gmail.com>
2015-06-26 15:22:21 +0100

    Removed old files

--- src/bookmarklet-final.js
+++
@@ -1,9 +0,0 @@
-javascript: (function() {
-    s = document.createElement("script");
-    s.type = "text/javascript";
-    // Dev testing source
-    s.src =  'http://trovalow.dev/bookmarklet/dist/js/trovalow-bookmarklet.min.js';
-    // Live Source
-    // s.src = 'http://trovalow.com/bookmarklets/trovalow-bookmarklet/dist/js/bookmarklet-external.min.js';
-    document.body.appendChild(s);
-})();
