beaf765b9f5511568e3ccf6f871145badcfd0f30
Aidan Smyth <smyth.aidan@gmail.com>
2015-06-26 15:19:49 +0100

    Added bower ask and components

---
+++ gulp/tasks/bower.js
@@ -0,0 +1,40 @@
+/*
+  Scripts tasks
+  --------------------------------------------------------------------
+
+*/
+
+// Load requirements
+// ------------------------------
+var gulp          = require('gulp'),
+    gutil         = require('gulp-util'),
+    plumber       = require('gulp-plumber'),
+    notify        = require('gulp-notify'),
+    // specific task config
+    config        = require('../../gulpConfig').bower,
+    // specific task modules
+    bower = require('gulp-bower')
+;
+
+// Error handler
+// ------------------------------
+var onError = function(err) {
+  notify.onError({
+    title:    "Gulp",
+    subtitle: "Failure!",
+    message:  "Error: <%= error.message %>",
+    sound:    "Beep"
+  })(err);
+
+  this.emit('end');
+};
+
+// Tasks
+// ------------------------------
+
+// Browser sync task
+gulp.task('bower', function() { 
+  return bower()
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(gulp.dest(config.src));
+}); 
