d2ea48f7bc1810f13a38da7305c4829f5887f746
Aidan Smyth <smyth.aidan@gmail.com>
2015-06-26 15:53:05 +0100

    updated images task to actually process images

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -8,11 +8,39 @@
 // ------------------------------
 var gulp          = require('gulp'),
     gutil         = require('gulp-util'),
-    config        = require('../../gulpConfig').images
+    plumber       = require('gulp-plumber'),
+    notify        = require('gulp-notify'),
+    // specific task config
+    config        = require('../../gulpConfig').images,
+    // specific task modules
+    imagemin      = require('gulp-imagemin'),
+    changed       = require('gulp-changed'),
+    browserSync   = require('browser-sync')
 ;
+
+
+// Error handler
+// ------------------------------
+var onError = function(err) {
+  notify.onError({
+    title:    "Gulp",
+    subtitle: "Failure!",
+    message:  "Error: <%= error.message %>",
+    sound:    "Beep"
+  })(err);
+
+  this.emit('end');
+};
 
 
 // Tasks
 // ------------------------------
 
-gulp.task('images');
+gulp.task('images', function() {
+  return gulp.src(config.src)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(changed(config.dest)) // Ignore unchanged files
+    .pipe(imagemin()) // Optimize
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.reload({stream:true}));
+});
