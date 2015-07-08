468ae2848568ee1ead6a0f14e22f23e539bb79ab
Aidan Smyth <smyth.aidan@gmail.com>
2015-07-08 16:33:58 +0100

    Added basic owl carousel options

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -5,3 +5,15 @@
 // Variables
 // -------------------------------------------------------------------
 console.log("main.js loaded");
+
+jQuery(document).ready(function(){
+  jQuery(".owl-carousel").owlCarousel({
+    navigation: false,
+    slideSpeed: 300,
+    singleItem: true,
+    loop: true,
+    autoPlay: true,
+    autoPlayHoverPause: true,
+    navRewind: true
+  });
+});
