292d96588981c672e82e694abe57ccd8f4915d91
Aidan Smyth <smyth.aidan@gmail.com>
2015-07-09 15:44:23 +0100

    Added generator for top-bar navigation menus from site-data.son

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -6,6 +6,7 @@
 // -------------------------------------------------------------------
 console.log("main.js loaded");
 
+// Owl carousel settings
 jQuery(document).ready(function(){
   jQuery(".owl-carousel").owlCarousel({
     navigation: false,
@@ -17,3 +18,22 @@ jQuery(document).ready(function(){
     navRewind: true
   });
 });
+
+// Build menu from json
+jQuery(function () {     
+    
+  function parseMenu(ul, menu) {
+    for (var i=0;i<menu.length;i++) {
+      var li=jQuery(ul).append('<li><a href="'+menu[i].link+'">'+menu[i].name+'</a></li>');
+      if (menu[i].sub!==null) {
+        var subul=jQuery('<ul id="submenu'+menu[i].link+'"></ul>');
+        jQuery(li).append(subul);
+        parseMenu(jQuery(subul), menu[i].sub);
+      }
+    }
+  }
+
+  var menu=jQuery('#menu');
+  parseMenu(menu, JSON.menu);
+
+ });
